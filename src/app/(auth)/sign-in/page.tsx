628c730b7646479ed781366de31455071defa8a7
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2024-10-25 00:21:26 +0530

    Handling signin

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,131 @@
-import React from "react";
-const page = () => {
-return (
-    <div>Page</div>
-)
+'use client';
+
+import { Response } from '@/types/apiResponse';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
+import {useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { signIn } from 'next-auth/react';
+
+import { Button } from '@/components/ui/button';
+import {
+    Form,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/hooks/use-toast';
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { signInSchema } from '@/schemas/signInSchema';
+
+export default function SignUpForm() {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const router = useRouter();
+    const { toast } = useToast();
+
+    const form = useForm<z.infer<typeof signInSchema>>({
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+            identifier: '',
+            password: '',
+        },
+    });
+
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+        setIsSubmitting(true);
+        try {
+            const result = await signIn('credentials', {
+                redirect: false,
+                identifier: data.identifier,
+                password: data.password
+            });
+
+            if (result?.error) {
+                if (result.error === 'CredentialsSignin') {
+                    toast({
+                        title: 'Login Failed',
+                        description: 'Incorrect username or password',
+                        variant: 'destructive',
+                    });
+                } else {
+                    toast({
+                        title: 'Error',
+                        description: result.error,
+                        variant: 'destructive',
+                    });
+                }
+            }
+
+            if (result?.url) {
+                router.replace('/dashboard');
+            }
+        } catch (error) {
+            console.error('Error during sign-up:', error);
+
+            const axiosError = error as AxiosError<Response>;
+
+            // Default error message with fallback
+            let errorMessage = axiosError.response?.data.message || 'There was a problem with your sign-up. Please try again.';
+
+            toast({
+                title: 'Sign Up Failed',
+                description: errorMessage,
+                variant: 'destructive',
+            });
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome Back to True Feedback
+          </h1>
+          <p className="mb-4">Sign in to continue your secret conversations</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className='w-full' type="submit">Sign In</Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default page
